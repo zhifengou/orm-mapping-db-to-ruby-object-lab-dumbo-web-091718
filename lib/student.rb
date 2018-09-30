fbeb0e99f226a14002f8fc33d89bdf0676e65098
zhifeng ou <zhifengou@hotmail.com>
2018-09-30 04:00:38 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -79,7 +79,7 @@ class Student
       SELECT *
       FROM students
       WHERE grade = 10 
-      LIMIT #{number}
+      LIMIT ?
     SQL
      DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
