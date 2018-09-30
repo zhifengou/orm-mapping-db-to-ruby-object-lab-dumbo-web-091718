f70deaadf8bed91dca82e60a65deeecb89523008
zhifeng ou <zhifengou@hotmail.com>
2018-09-30 03:29:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,5 +58,11 @@ class Student
     DB[:conn].execute(sql)
   end
   
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT COUNT(*) FROM STUDENTS where grade = 9;
+    SQL
+     DB[:conn].execute(sql)
+  end
   
 end
