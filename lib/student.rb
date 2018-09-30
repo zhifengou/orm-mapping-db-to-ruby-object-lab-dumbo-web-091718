7548a1401c7648cbd6a779b6929fe7cf365e1eb6
zhifeng ou <zhifengou@hotmail.com>
2018-09-30 03:30:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,4 +65,11 @@ class Student
      DB[:conn].execute(sql)
   end
   
+    def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade < 12;
+    SQL
+     DB[:conn].execute(sql)
+  end
+  
 end
