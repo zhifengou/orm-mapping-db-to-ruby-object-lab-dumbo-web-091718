430b8142d2ea524848418f6c1e0e9c072f8c1b35
zhifeng ou <zhifengou@hotmail.com>
2018-09-30 03:48:49 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -86,5 +86,25 @@ class Student
     end
    end 
   
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      ORDER BY students.id ASC LIMIT 1
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+   def self.all_students_in_grade_X(x)
+     sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = #{x}
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   
 end
