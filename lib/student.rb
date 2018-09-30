16fec30790f8f7118448742bdfa62cef1a7d9d0f
zhifeng ou <zhifengou@hotmail.com>
2018-09-30 02:54:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,6 +17,8 @@ class Student
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql=<<-SQL 
+      SELECT 
   end
   
   def save
