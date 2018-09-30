c423bb8d085863198fede9e21040816e03ed1060
zhifeng ou <zhifengou@hotmail.com>
2018-09-30 02:56:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,7 @@ class Student
     # find the student in the database given a name
     # return a new instance of the Student class
     sql=<<-SQL 
-      SELECT 
+      SELECT * FROM students WHERE name=
   end
   
   def save
