43822745a59ef6b9087c57a65ca48683377f0025
zhifeng ou <zhifengou@hotmail.com>
2018-09-30 03:13:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,6 +3,11 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    student = Student.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
   end
 
   def self.all
