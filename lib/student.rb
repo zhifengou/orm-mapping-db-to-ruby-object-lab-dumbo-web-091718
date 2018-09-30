7ef26b99e817e770a41b6d2b51e75f2f7ecf89a8
zhifeng ou <zhifengou@hotmail.com>
2018-09-30 04:00:08 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -93,11 +93,11 @@ class Student
    self.new_from_db(DB[:conn].execute(sql)[0])
   end
   
-   def self.first_X_students_in_grade_10(x)
+   def self.all_students_in_grade_X(x)
      sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade = 10
+      WHERE grade = ?
     SQL
     DB[:conn].execute(sql,x)
 end
